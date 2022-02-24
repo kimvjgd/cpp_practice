2f373c91f1c9c714a07206bed666c2eb70f0804b
semidongdong <semidongdong@gmail.com>
2022-02-24 20:55:00 +0900

    today last

---
+++ chapter04/Circle.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+class Circle
+{
+public:
+  int radius;
+  string color;
+
+  double calcArea()
+  {
+    return 3.14 * radius * radius;
+  }
+};
---
+++ chapter04/ex01_class.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "Circle.cpp" // "" : 현재 디렉토리에서 먼저 찾아라
+// #include <Circle.cpp>  // 라이브러리 디렉토리에서 찾아라
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  Circle obj;
+  obj.radius = 100;
+  obj.color = "blue";
+
+  cout << "원의 면적 " << obj.calcArea() << endl;
+  return 0;
+}
