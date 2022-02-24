624795df1835ead5f6db0b2849d532587cb694c9
semidongdong <semidongdong@gmail.com>
2022-02-24 20:57:26 +0900

    zz

--- chapter04/ex01_class.cpp
+++ chapter04/ex01_class.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
 #include "Circle.cpp" // "" : 현재 디렉토리에서 먼저 찾아라
 // #include <Circle.cpp>  // 라이브러리 디렉토리에서 찾아라
-using namespace std;
 
 int main(int argc, char const *argv[])
 {
